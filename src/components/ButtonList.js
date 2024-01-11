53ef9366c3dbb5bb05919c91a71376fc658491c9
richeshS <156299886+richeshS@users.noreply.github.com>
2024-01-11 14:45:05 +0530

    Add files via upload

---
+++ src/components/ButtonList.js
@@ -0,0 +1,17 @@
+import Button from "./Button";
+
+const ButtonList = () => {
+    return (
+        <div className="flex">
+            <Button name="All"/>
+            <Button name="Gaming"/>
+            <Button name="Songs"/>
+            <Button name="Live"/>
+            <Button name="Cricket"/>
+            <Button name="Cooking"/>
+            <Button name="Football"/>
+            <Button name="Jetha Bhai"/>
+        </div>
+    );
+};
+export default ButtonList;
---
+++ src/components/WatchPage.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../utils/appSlice";
+import { useParams, useSearchParams } from "react-router-dom";
+
+const WatchPage = () => {
+    const [searchParams] = useSearchParams();
+
+
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(closeMenu());
+    })
+    return (
+        <div>
+            <iframe width="1200" height="600" src={"https://www.youtube.com/embed/" + searchParams.get("v")} 
+            title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        </div>
+    );
+};
+
+export default WatchPage;
